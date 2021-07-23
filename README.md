68f074832a452bd45b9e26debf9ed22deecb8b3d
Kalin Nonchev <50597791+KalinNonchev@users.noreply.github.com>
2021-07-23 18:36:39 +0200

    init

---
+++ README.md
@@ -0,0 +1,47 @@
+# WOKO_alert
+##### Any feedback or contribution would be appreciated. Don't forget to give a STAR :star2:
+##### Currently, this is a code base with minimum condition requirements. CS guys could help if something more sophisticated is needed. ;)
+The script checks the [WOKO](http://www.woko.ch) website for student accommodation in Zurich, Switzerland for new room entries every 6 to 12 min and sends you an email alert if a new room is **available**.
+
+## Usage
+
+Please, navigate to the **WOKO_alert** folder
+
+### 1. Install dependencies 
+
+Open your favourite command line (Terminal etc) and run this line to install the missing dependencies:
+```
+pip install -r requirements.txt
+```
+
+### 2. Fill config file
+
+Fill the config yaml file
+
+```python
+content: "New WOKO room!"
+receiver_email: "pass" # email to send to 
+sender_email: "pass" # email to send from @gmail
+password: "pass" # paswword of the sender_email email @gmail
+keyword: "Zürich" # Choose between Zürich and Winterthur
+```
+
+You can use the same **receiver_email** and **sender_email**.  
+I recommend creating a second special email (sender_email) for the purpose of this app. \
+You **have** to use gmail and you **have** to turn **Less secure app access** feature **on**. Read more [here](https://support.google.com/accounts/answer/6010255?hl=en#zippy=%2Cif-less-secure-app-access-is-on-for-your-account).
+
+### 3. Run
+
+Open your favourite command line (Terminal etc) and run the script as: 
+
+```python
+python3 queryWOKO.py
+```
+
+If everything is fine, you should see something like:
+```
+Still: 2 rooms...
+Sleep for: 6min.
+```
+Leave the script running in the background. The script will update itself every 6 to 12 min and if a new room is available, it will notify you by email. Enjoy! \
+**Good luck!**
---
+++ queryWOKO.py
@@ -0,0 +1,83 @@
+from urllib.request import urlopen
+import ssl
+import smtplib
+import time
+import random
+from bs4 import BeautifulSoup
+import yaml
+
+with open("config.yaml", "r") as opened_file:
+    config = yaml.safe_load(opened_file)
+
+
+def send_message(content, receiver_email, sender_email, password):
+    """
+    Send email if the number of rooms on WOKO is changed
+    :param content:
+    :param receiver_email:
+    :param sender_email:
+    :param password:
+    :return:
+    """
+    port = 587  # For starttls
+    smtp_server = "smtp.gmail.com"
+    message = f"Subject: You have a new post\n\n\n{content}.\n\n\nCheers,\nYour team"
+    print(message)
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+
+    print('Message send!')
+
+def query_all_website():
+    """
+    Check the WOKO website for the number of available rooms
+    :return: Number of rooms
+    """
+    url = "http://www.woko.ch/en/nachmieter-gesucht"
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html, features="html.parser")
+
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+
+    # get text
+    text = soup.get_text()
+
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = [chunk for chunk in chunks if config["keyword"] in chunk]
+    return text
+
+def sleep():
+    """
+    Sleep time
+    between 6 to 12 minutes
+    :return:
+    """
+    timer = 360 * random.choice([1, 2])
+    print(f"Sleep for: {timer // 60}min.")
+    time.sleep(timer)
+
+
+memory_list = query_all_website()
+
+while True:
+    new_memory_list = query_all_website()
+
+    if memory_list != new_memory_list:
+        send_message(**config)
+        print("Found!")
+        memory_list = new_memory_list
+        sleep()
+    else:
+        print(f"Still: {len(new_memory_list) - 3} rooms...")
+        sleep()
